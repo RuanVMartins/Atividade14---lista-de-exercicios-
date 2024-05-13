126c5d794c51965b89ced215ae6e038ff48db58a
RuanVMartins <160753234+RuanVMartins@users.noreply.github.com>
2024-05-13 12:48:32 -0300

    Add files via upload

---
+++ atv 13/script.js
@@ -0,0 +1,51 @@
+function sasa(){
+    const numUm = 5;
+    const numDois = 3;
+    let resultado = (numUm - numDois);
+    window.alert("A diferença entro os numeros é "+ resultado);
+
+}
+
+function dobro(){
+    const numUm = 5;
+    const numDois = 3;
+let resultado2 = (numUm * 2 + numDois * 3);
+window.alert("O dobro da primeira mais o triplo da segunda variáve é "+ resultado2);
+}
+
+function dudu(){
+    const numUm = 5;
+    const numDois = 3;
+let resultado3 = (numUm * numDois);
+window.alert("A multiplicação das duas variaveis é "+ resultado3);
+}
+
+function baba(){
+    const numUm = 5;
+    const numDois = 6;
+    if (numUm > numDois) {
+        window.alert("o numero maior é "+ numUm);
+    } else {
+        window.alert("o numero maior é "+ numDois);
+    }
+
+}
+
+function susu(){
+    var nome = parseInt(document.getElementById('nome').value);
+}
+
+function sasu(){
+    var salario = parseInt(document.getElementById('nome').value);
+}
+
+function desc(){
+    var descont = parseInt(document.getElementById('nome').value);
+}
+
+
+
+
+
+
+
